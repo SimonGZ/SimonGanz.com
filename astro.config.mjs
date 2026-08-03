c91a82111d1d7769a1c1d4a296f701873bf1106a
SimonGZ <dev@simonganz.com>
2026-08-03 12:50:47 -0700

    Use Astro native Markdown processor

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-import { unified } from '@astrojs/markdown-remark';
-import { remarkExcerpt } from './remark-excerpt.mjs';
 
 // https://astro.build/config
 export default defineConfig({
-  markdown: {
-    processor: unified({
-      remarkPlugins: [remarkExcerpt]
-    })
-  },
   integrations: [mdx()]
 });
